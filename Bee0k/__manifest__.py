5358ea1edbdf80946d92a6fd451428857586f56b
jnc <jnc@odoo.com>
2021-01-30 13:10:14 +0100

    [Bee0k Module template]

---
+++ Bee0k/__manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Bee0k',
+    'version': '1.0.0',
+    'summary': 'Bee0k',
+    'description': """
+Bee0k Template customisation
+""",
+    'depends': [
+    ],
+    'data': [
+        # views
+        # datas
+        # security
+        # wizard
+        #reports
+    ],
+    'qweb': [
+    ],
+    'test': [
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+}
